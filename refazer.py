904700daa9da07214962f556f4a51fc4cd16129d
mayson <maysonax@gmail.com>
2024-06-20 18:41:11 +0100

    lista de afazeres
    muito parecido com a lista de compras,  mas agora você pode desfazer e também refazer.

---
+++ refazer.py
@@ -0,0 +1,73 @@
+# Exercício - Lista de tarefas com desfazer e refazer
+# Música para codar =)
+# Everybody wants to rule the world - Tears for fears
+# todo = [] -> lista de tarefas
+# todo = ['fazer café'] -> Adicionar fazer café
+# todo = ['fazer café', 'caminhar'] -> Adicionar caminhar
+# desfazer = ['fazer café',] -> Refazer ['caminhar']
+# desfazer = [] -> Refazer ['caminhar', 'fazer café']
+# refazer = todo ['fazer café']
+# refazer = todo ['fazer café', 'caminhar']
+import os
+def mostrar_lista(lista):
+    print('=' * 20)
+    print(f'{"LISTA":^20}')
+    print('-' * 20)
+    if not lista:
+        print(f'{"LISTA VAZIA":^20}')
+    else:
+        for item in lista:
+            l= f'{item:^20}'
+            print(l.upper())
+    print('=' * 20)
+    print()
+
+
+def refazer():
+    global historico
+    if historico:
+        tudo.append(historico.pop())
+        mostrar_lista(tudo)
+        print()
+        return
+    print('não há oque refazer. ')
+    mostrar_lista(tudo)
+    print()
+
+
+def desfazer():
+    global historico
+    global tudo
+    if tudo:
+        historico.append(tudo.pop())
+        mostrar_lista(tudo)
+        print()
+        return
+    print('não há oque desfazer.')
+    mostrar_lista(tudo)
+    print()
+
+
+
+tudo = []
+historico =[]
+comando = [ 'listar', 'desfazer', 'refazer']
+while True:
+    print('Digite um comando')
+    acao = input('listar | desfazer | refazer: ').lower().strip()
+    os.system('cls')
+    if acao == '':
+        print('digite algo valido!')
+        continue
+    if acao in comando:
+        match acao:
+            case 'listar':
+                mostrar_lista(tudo) 
+            case 'desfazer':
+                desfazer()
+            case 'refazer':
+                refazer()
+    else:
+        tudo.append(acao)
+        historico.clear()
+        mostrar_lista(tudo)
